3a46ab95bb173fd1a1024531c2a688e520e5382b
Borisevich V <vasily.borisevich@gmail.com>
2024-03-21 13:26:12 +0300

    Почистил мусор. Добавил комменты.

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import re
 import os
 from time import sleep
 
-#list_of_values="1
-# 2
-# 3
-# 4"
-
-#obiazatpole="0 or 1"
-
-#user_tree_element_name="жалобы"
-
+# типы элементов
 elemet_types = {0 : 'папка (группа)',
                 1 : 'список из БД',
                 2 : 'форматируемый текст (большое поле)',
@@ -30,58 +22,63 @@ elemet_types = {0 : 'папка (группа)',
                 16 : 'галочки (чек боксы)',
                 17 : 'список с галочками (чек боксами)'}
 
-elemet_parameters = ['list_of_values', 'obiazatpole', 'user_tree_element_name']
-
+# функция логгирования
 def log(text:str, filename:str)-> bool:
     with open(f'./after/{filename}.txt', 'a', encoding='utf8') as file:
         file.write(text)
         print(text)
             
 
+# перебираем все файлы из папки before
 for filename in os.listdir('./before/'):
-    if (os.path.isfile(f'./before/{filename}') is False) or (filename == '.gitkeep'):
+    if (os.path.isfile(f'./before/{filename}') is False) or (filename == '.gitkeep'): # если не файл или .getkeep, то скипаем
         continue
 
+    # открываем файл из папки before
     with open(f'./before/{filename}', 'r', encoding='utf-8') as file:
         
-        i = 1
-        lin = 1
-        elist = 1
-        val_exist = 1
-        count = 0
-        type_nn = 0
+        i = 1           # номер строки в файле
+        lin = 1         # бул на проверку пустых значений в поле
+        elist = 1       # бул на проверку конца списка
+        val_exist = 1   # бул на проверку элемента
+        count = 0       # счётчик найденных элементов
+        type_nn = 0     # номер типа элемента (0 - папка)
 
+        # переборка файла
         for line in file:
 
+            # поиск начала элемента "<Element_1..."
             res = re.match(r'.*<Elem.* P', line)
             if res is not None:
                 count = count + 1
-                type_nn = re.search(r'TYP="[0-9]*"', line).group(0)[5:-1]
-            if int(type_nn) != 0:
+                type_nn = re.search(r'TYP="[0-9]*"', line).group(0)[5:-1]   # присваиваем тип элемента
+            if int(type_nn) != 0:   # если не папка
 
-                # is_required = re.search('obiazatpole')
-
-                tree_fiel_name = re.search(r'user_tree_element_name=".*".*prog', line)
+                tree_fiel_name = re.search(r'user_tree_element_name=".*".*prog', line)  # имя элемента в дереве шаблонизатора
                 
-                full_field_name = re.search(r'nadpis_text=".*".*nadpis', line)
+                full_field_name = re.search(r'nadpis_text=".*".*nadpis', line)  # имя в label элемента
+
+                # обрезаем распарсенные данные до значений
                 if full_field_name is not None:
                     field_name = re.search(r'".*"', full_field_name.group(0))
                     t_field_name = re.search(r'".*"', tree_fiel_name.group(0))
+                    # пишем в файл название элемента v
                     log(f'\nНазвание поля -> "{field_name.group(0)[1:-21]}" ({elemet_types[int(type_nn)]}) (переменная: {t_field_name.group(0)[1:-1]})\n', filename)
 
+                # ищем список значений
                 res = re.search(r'listofvalues=".*', line)
                 if res is not None:
-                    line = re.search(r's=".*', res.group(0)).group(0)[3:]
+                    line = re.search(r's=".*', res.group(0)).group(0)[3:]   # получаем первое значение
                     val_exist = 0
 
-                list_is_none = re.search(r'listofvalues=""', line)
+                list_is_none = re.search(r'listofvalues=""', line)  # если пусто, то скипаем
                 if list_is_none is None:
                     lin = 0
                 else:
                     lin = 1
                     type_nn = 0
 
-                end_list = re.search(r'".*height', line) # тут нихуя не работает почему-то блять
+                end_list = re.search(r'".*height', line) # проверка конца списка значений
                 if end_list is not None:
                     elist = 1
                     type_nn = 0
@@ -89,6 +86,7 @@ for filename in os.listdir('./before/'):
                 else:
                     elist = 0
                     
+                # если все звёзды сошлись, то пишем строку в файл
                 if lin == 0 and elist == 0 and val_exist == 0:
                     log(line, filename)
 
